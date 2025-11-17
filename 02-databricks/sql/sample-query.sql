a0a113f8b45e428c03fd60581dcc1005e5b6a726
Gustavo Erthal <gustavoerthaljr@gmail.com>
2025-11-17 12:19:25 -0300

    Creating samples files

---
+++ 02-databricks/sql/sample-query.sql
@@ -0,0 +1,7 @@
+SELECT
+    customer_id,
+    SUM(total_amount) AS total_spent,
+    COUNT(*) AS orders
+FROM sales_clean
+GROUP BY customer_id
+ORDER BY total_spent DESC;
---
+++ 03-dbt/models/staging/stg-products.sql
@@ -0,0 +1,14 @@
+with source as (
+    select * from {{ source('ecommerce', 'products') }}
+),
+
+clean as (
+    select
+        product_id,
+        name,
+        category,
+        cast(price as decimal(10,2)) as price
+    from source
+)
+
+select * from clean
